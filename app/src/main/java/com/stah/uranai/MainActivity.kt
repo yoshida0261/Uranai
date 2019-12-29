258aab0b14ef88574815402a2344edbe914ce591
yoshida0261 <yoshida261@gmail.com>
2019-12-30 01:59:44 +0900

    feat: 消えて表示のアニメーションを実装

--- app/src/main/java/com/stah/uranai/MainActivity.kt
+++ app/src/main/java/com/stah/uranai/MainActivity.kt
@@ -1,8 +1,9 @@
 package com.stah.uranai
 
+import android.animation.AnimatorSet
 import android.animation.ObjectAnimator
 import android.os.Bundle
-import android.view.View.ALPHA
+import android.view.View
 import android.view.animation.AnimationUtils
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
@@ -14,13 +15,43 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        ObjectAnimator.ofFloat(daikiti, "alpha", 0.0f)
+            .start()
+
         imageView.setImageResource(R.drawable.panda1)
 
         //findViewById(R.id.monster).startAnimation(AnimationUtils.loadAnimation(this, R.anim.a1));
         imageView.startAnimation(AnimationUtils.loadAnimation(this, R.anim.updown))
         imageView.setOnClickListener {
-            val fadeAltAnim = ObjectAnimator.ofFloat(it, "alpha", 0.0f)
-            fadeAltAnim.start()
+
+            val panda = ObjectAnimator.ofFloat(it, "alpha", 0.0f)
+                .setDuration(1000)
+
+
+            val jinja1 = ObjectAnimator.ofFloat(backgroundJinja, "alpha", 0.0f)
+                .setDuration(1000)
+
+            val anime = AnimatorSet()
+            anime.playTogether(panda, jinja1)
+
+
+
+
+            val daikiti = ObjectAnimator.ofFloat(daikiti, "alpha", 0.1f, 1.0f)
+                .setDuration(3000)
+            val jinja2 = ObjectAnimator.ofFloat(backgroundJinja, "alpha", 0.1f, 1.0f)
+                .setDuration(3000)
+
+
+            //anime.playSequentially(anime2)
+            anime.start()
+
+            val anime2 = AnimatorSet()
+            anime2.playTogether(daikiti, jinja2)
+            anime2.startDelay = 1000
+            anime2.start()
+
+
         }
 
 //        val fadeAnim = ObjectAnimator.ofFloat(imageView, "alpha", 0.2f)
