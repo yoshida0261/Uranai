59626ec0c13ed646d14c8315e0e6aaccb918a914
yoshida0261 <yoshida261@gmail.com>
2019-12-30 02:02:53 +0900

    refactor: brash up

--- app/src/main/java/com/stah/uranai/MainActivity.kt
+++ app/src/main/java/com/stah/uranai/MainActivity.kt
@@ -3,7 +3,6 @@ package com.stah.uranai
 import android.animation.AnimatorSet
 import android.animation.ObjectAnimator
 import android.os.Bundle
-import android.view.View
 import android.view.animation.AnimationUtils
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
@@ -15,18 +14,16 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // 後で表示するので事前にalphaを０にする
         ObjectAnimator.ofFloat(daikiti, "alpha", 0.0f)
             .start()
 
-        imageView.setImageResource(R.drawable.panda1)
+        panda.startAnimation(AnimationUtils.loadAnimation(this, R.anim.updown))
+        panda.setOnClickListener {
 
-        //findViewById(R.id.monster).startAnimation(AnimationUtils.loadAnimation(this, R.anim.a1));
-        imageView.startAnimation(AnimationUtils.loadAnimation(this, R.anim.updown))
-        imageView.setOnClickListener {
-
+            // アニメーションはObjectanimatorで処理　https://techblog.yahoo.co.jp/programming/androidiphone/
             val panda = ObjectAnimator.ofFloat(it, "alpha", 0.0f)
                 .setDuration(1000)
-
 
             val jinja1 = ObjectAnimator.ofFloat(backgroundJinja, "alpha", 0.0f)
                 .setDuration(1000)
@@ -34,27 +31,19 @@ class MainActivity : AppCompatActivity() {
             val anime = AnimatorSet()
             anime.playTogether(panda, jinja1)
 
-
-
-
             val daikiti = ObjectAnimator.ofFloat(daikiti, "alpha", 0.1f, 1.0f)
                 .setDuration(3000)
             val jinja2 = ObjectAnimator.ofFloat(backgroundJinja, "alpha", 0.1f, 1.0f)
                 .setDuration(3000)
-
-
-            //anime.playSequentially(anime2)
             anime.start()
 
+            // 簡単なanimeなのでタイミングは手入力
             val anime2 = AnimatorSet()
             anime2.playTogether(daikiti, jinja2)
             anime2.startDelay = 1000
             anime2.start()
 
-
         }
 
-//        val fadeAnim = ObjectAnimator.ofFloat(imageView, "alpha", 0.2f)
-        //      fadeAnim.start()
     }
 }
